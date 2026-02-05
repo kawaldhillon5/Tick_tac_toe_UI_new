89e694681694f66abedf47a6b99a8a79325e6d01
kawal <kawaldhillon5@gmail.com>
2026-02-05 01:20:59 -0500

    created router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GameProvider } from './contexts/gameContext'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Home } from './routes/Home';
+import { Game } from './routes/Game';
+import Root from './routes/Root';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <GameProvider><Root/></GameProvider>,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "game/:gameId", 
+        element: <Game />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GameProvider>
-        <App />
-    </GameProvider>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
