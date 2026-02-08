c06cc20e882499098d3a4575c25b0f4f256fd056
kawal <kawaldhillon5@gmail.com>
2026-02-07 23:32:03 -0500

    modified

--- src/types/socket.ts
+++ src/types/socket.ts
@@ -1,19 +1,29 @@
 import { Socket } from "socket.io-client";
+import type { Board, Player } from "./game";
 
 // 1. Events the CLIENT sends to the SERVER
 export interface ClientToServerEvents {
   join_queue: () => void;
   leave_queue: () => void;
-  make_move: (data: { gameId: string; index: number }) => void;
+  join_game : (data:{gameId: string}) => void,
+  make_move: (data: { gameId: string; row: number, col: number, player: string }) => void;
 }
 
 // 2. Events the SERVER sends to the CLIENT
 export interface ServerToClientEvents {
   session: (data: { gamerId: string; }) => void;
-  queue_joined: (data: {gameId:string;})=>void;
   game_start: (data: { gameId: string;}) => void;
-  game_update: (data: { board: string[]; currentTurn: string; }) => void;
-  game_over: (data: { winnerId: string | null; winningLine: number[] | null; }) => void;
+  game_state: (data: { 
+    gameId: string;
+    board: Board;
+    currentTurn: string | null;
+    opponent: Player | null;
+    status: 'waiting' | 'ongoing' | 'won' | 'draw';
+    winner: string | null;
+    winningArray: {row:number, col: number}[] | null
+  }) => void;
+  game_update: (data: { board: Board; currentTurn: string | null }) => void;
+  game_over: (data: { status: 'won' | 'draw',  winnerId: string | null; winningArray: {row:number, col: number}[] | null }) => void;
   error: (data: { message: string;}) => void;
 }
 
